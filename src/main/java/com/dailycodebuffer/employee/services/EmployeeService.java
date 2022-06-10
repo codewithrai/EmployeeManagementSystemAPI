078b8c032da60cc75afcdcd0f32176ce23a73428
Waqar Ahmad <engrwaqar37@gmail.com>
2022-06-10 23:31:29 +0500

    Fetching all employees with API functionality completed

--- src/main/java/com/dailycodebuffer/employee/services/EmployeeService.java
+++ src/main/java/com/dailycodebuffer/employee/services/EmployeeService.java
@@ -3,6 +3,10 @@ package com.dailycodebuffer.employee.services;
 
 import com.dailycodebuffer.employee.model.Employee;
 
+import java.util.List;
+
 public interface EmployeeService {
     public Employee createEmployee(Employee employee);
+
+    public List<Employee> getAllEmployees();
 }
--- src/main/java/com/dailycodebuffer/employee/services/EmployeeServiceImpl.java
+++ src/main/java/com/dailycodebuffer/employee/services/EmployeeServiceImpl.java
@@ -6,6 +6,9 @@ import com.dailycodebuffer.employee.repository.EmployeeRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
@@ -21,5 +24,16 @@ public class EmployeeServiceImpl implements EmployeeService {
         BeanUtils.copyProperties(employee, employeeEntity);
         repository.save(employeeEntity);
         return employee;
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        List<EmployeeEntity> employeeEntities = repository.findAll();
+        List<Employee> employees = employeeEntities
+                .stream()
+                .map(e -> new Employee(e.getId(),
+                        e.getFirstName(), e.getLastName(), e.getEmailId()))
+                .collect(Collectors.toList());
+        return employees;
     }
 }
